08992c8d5ac659d34516612b1d656d4d7b9624bb
saikumar <saikumarkaleru56@gmail.com>
2021-07-24 14:11:34 +0530

    c

---
+++ CSE SUBJECTS/Java/Revise/Generics.java
@@ -0,0 +1,17 @@
+package Revise;
+public class Generics {
+
+    /*
+    https://howtodoinjava.com/java/generics/complete-java-generics-tutorial/
+
+    Java Generics are a language 
+    feature that allows for definition and use of generic types and methods
+
+    */
+
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
---
+++ CSE SUBJECTS/Java/programs/practice.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class practice {
+    
+
+public static void main(String[] args) {
+    
+String s="sai";
+char arr[]=s.toCharArray();
+
+Arrays.sort(arr);
+
+System.out.println(arr);
+}
+
+}
